48e8e047324a72db76b8ee810d33f1781ea9478d
theresaswayne <tcs6@columbia.edu>
2017-07-21 17:54:47 -0400

    reformat results -- works to collate length measurements

--- reformat_results.py
+++ reformat_results.py
@@ -6,8 +6,9 @@ Created on Thu Jul 20 16:48:37 2017
 @author: confocal
 """
 
+# assumes no headers, 5 columns
 
-import pylab
+import pylab, os, csv
 
 def getData(fileName):
     ''' takes a csv filename
@@ -17,26 +18,67 @@ def getData(fileName):
     dataFile = open(fileName, 'r')
     imageName = ""
     lengths = []
-    discardHeader = dataFile.readline()
+#    discardHeader = dataFile.readline() # variable is not used -- this just gets rid of the header
     # or         header = f.readline().strip().split(',')
     for line in dataFile:
-        row,image,x,y,angle,cellLength = line.split(",")
+        image,x,y,angle,cellLength = line.split(",")
         # or             items = line.strip().split(',')
         imageName = image # TODO: gather this just once for efficiency
         lengths.append(float(cellLength)) # gathers the length of the line
-    dataFile.close()
+    dataFile.close() # you have to close it or nobody can use it
     return (imageName, lengths)
 
-# TODO: make lengths into an array of 3 columns
+path = "/Users/confocal/Desktop/input/" 
 
-# TODO: write csv file containing these values and titled the same as the image
-     
-# TODO: replace with useful values   
-path = "/Users/confocal/Desktop/2017-05 resonant scanner/"
-file1 = path+"reso2.csv"
-label1 = "Resonant"
-file2 = path+"galvo2.csv"
-label2 = "Galvo"
-getData(file1)
-pylab.show()
+for i in range (1,8):
+    
+    filename = "MAX_psy265 36c 001_xy"+str(i)+" lengths.csv"
+    file1 = path+filename
+    
+    name, cellLengths = getData(file1)
+    
+    print("name of the file is",name) 
+    # TODO: take off the slice number
+    # print("harvested the following lengths",cellLengths)
+    
+    
+    #def chunks(l, n):
+    #    n = max(1, n)
+    #    return (l[i:i+n] for i in xrange(0, len(l), n))
+    
+    groupedLengths = [cellLengths[i:i+3] for i in range(0, len(cellLengths), 3)]
+    #print("I grouped the lengths in 3s:",groupedLengths)
+    
+    # TODO: create csv file titled the same as the image
+    # write headers
+    # filename, cell number, mother, bud initial, bud final [eventually b:m initial, b:m final]
+    
+    # setup output file
+    csvPath = path + os.sep + "collated_lengths_"+str(i)+".csv"
+    csvExists = os.path.exists(csvPath)
+    csvFile = open(csvPath, 'a') # creates the file. a for append (b not necessary in python 3)
+    csvWriter = csv.writer(csvFile) # this object is able to write to the output file
+    
+    # add headers to output file
+    if not csvExists: # avoids appending multiple headers
+        headers1 = ['Filename','cell number','mother diam um','bud initial um', 'bud final um']
+        csvWriter.writerow(headers1)
+    else:
+        print("Appending to existing file.")
+        
+    cellNum = 0
+    for group in groupedLengths:
+        cellNum += 1
+        collectedData = [name, cellNum]
+        for value in group:
+            collectedData.append(value)
+        print("the next line is",collectedData)
+        csvWriter.writerow(collectedData)
+    
+    csvFile.close() 
+    print("finished field",i)
 
+#TODO: get ratios by using arrays
+
+
+print("finished")
