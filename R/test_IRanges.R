caac869596226a74d9826345f05ba7fdf9584e08
Theresa Swayne <theresa@Mini-Swemtage.local>
2018-03-04 16:22:01 -0500

    iRanges -- played with methods

--- R/test_IRanges.R
+++ R/test_IRanges.R
@@ -3,10 +3,12 @@
 # for possible use with find_redundant_domains.R
 
 library(IRanges)
+library(S4Vectors)
+library(stats4)
 
 # sample data representing start and end of made-up protein domains
-starts <- c(1, 2, 10, 2)
-ends <- c(5, 6, 20, 200)
+starts <- c(10, 20, 100, 20)
+ends <- c(50, 60, 200, 2000)
 
 x <- IRanges(start=starts, end=ends)
 
@@ -14,7 +16,12 @@ que <- x[1]
 
 subj <- x[2:4]
 
-ove <- findOverlaps(que, subj)
+ove <- findOverlaps(x, x)
 
-nea <- nearest(x = que, subject = subj)
+nea <- nearest(x, x)
+
+d2nea <- distanceToNearest(x, x, select = "arbitrary")
+
+dista <- distance(x, x)
+
 
