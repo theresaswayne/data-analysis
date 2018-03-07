5f033eadd1c03d284d677e6d32d2e547073c1d30
theresaswayne <tcs6@columbia.edu>
2018-03-07 17:14:16 -0500

    count domains -- simplified filtering, added data table on interpro PIDs

--- R/count_domains.R
+++ R/count_domains.R
@@ -1,25 +1,28 @@
 # count_domains.R
 # Script to count number of detected domains per protein
 # in records downloaded from SGD
+# filtered by a list of accession numbers denoting PIDs (protein interaction domains)
 
-# Input: CSV file from SGD with raw headers
-# Must include Protein systematic name (secondary identifier), 
-# and all possible fields under protein domains.
-# Assumes that the protein contains some domains.
+# Input: 
+# 1) CSV file from SGD with raw headers; 
+#   must include Protein systematic name (secondary identifier), 
+#   and all possible fields under protein domains.
+#   Assumes that the protein contains some domains.
+# 2) CSV file of accession numbers for filtering
 
-# Output:
-# 1. Frequency distribution table and histogram of domains per protein
-# 2. Sorted table of proteins by # domains detected (1 row per protein)
+# Output (Note: over-writes without warning!):
+# 1. Histogram of PIDs per protein
+# 2. Sorted table of proteins by frequency of PIDs detected (1 row per protein)
+# 3. Table of protein domains filtered on PIDs (including only the PIDs)
 
 # Next steps:
 # -- eliminate redundant domains using start and end residues
-# -- filter by domains that are involved in protein-protein interactions
 
 # Refinements:
-# -- prompt user for file
+# -- prompt user for files
 # -- check for required columns
 # -- rename columns in summary to something sensible
-# -- format histogram nicely
+# -- don't create so many new dataframes, but mutate instead (e.g. when sorting)
 
 library(dplyr)
 library(readr)
@@ -35,50 +38,36 @@ pidfile <- "PID_accessions_only.csv"
 # unlike base R's read.csv, readr's read_csv gives a tbl that can be grouped
 proteins <- read_csv(file.path(datapath, datafile)) 
 
-# a list of accession numbers, with header "Accession"
-pids <- read_csv(file.path(datapath, pidfile))
+# Filter to count only PIDs -----------------------------------------------
 
-# change tbl to vector
-pids_acc <- pids$Accession
-
+pids <- read_csv(file.path(datapath, pidfile)) # a list of accession numbers, with header "Accession"
+pids_acc <- pids$Accession # change tbl to vector
 pid_proteins <- filter(proteins, Gene.proteins.proteinDomains.name %in% pids_acc)
 
-# Group by protein name ---------------------------------------------------
+# Count rows bearing each protein name  and sort by # domains --------
 
-proteins_by_protname <- group_by(proteins, proteins$`Gene.proteins.secondaryIdentifier`)
+# grouped table = easier to count rows within a category
 pid_proteins_by_name <- group_by(pid_proteins, pid_proteins$`Gene.proteins.secondaryIdentifier`)
-
-# Count rows bearing each protein name  --------
-
-domains_per_protein <- summarize(proteins_by_protname, n())
 pid_domains_per_protein <- summarize(pid_proteins_by_name, n())
+pid_inorder <- arrange(pid_domains_per_protein, desc(`n()`)) # backticks because of parentheses in column name
 
-# use backticks around the column name because it has parentheses
-inorder <- arrange(domains_per_protein, desc(`n()`))
-pid_inorder <- arrange(pid_domains_per_protein, desc(`n()`))
-
-# Create and save histogram of domains per protein ---------------------------------
-
-outputgraph <- file.path(datapath,"domains_per_prot_redundant.pdf")
-pdf(file = outputgraph)
-hist(domains_per_protein$`n()`, 
-     col = "blue", 
-     main="Domains per protein including redundancies", 
-     xlab="domains identified in SGD")
-dev.off() # closes pdf output
+# Histogram of PIDs per protein ---------------------------------
 
 pid_outputgraph <- file.path(datapath,"pid_domains_per_prot_redundant.pdf")
 pdf(file = pid_outputgraph)
-hist(pid_domains_per_protein$`n()`, 
-     col = "blue", 
+pid_freqs <- pid_domains_per_protein$`n()`
+hist(pid_freqs,
+     breaks = seq(1:max(pid_freqs)),
+     col = "blue",
      main="PIDs per protein including redundancies", 
      xlab="PIDs identified in SGD")
 dev.off() # closes pdf output
 
-# Save data table -----------------------------------------------
+# Data tables -----------------------------------------------
 
-outputfile <- "proteins_by_number_domains.csv"
-write.csv(inorder, file.path(datapath,outputfile))
+freq_outputfile <- "proteins_by_number_pids.csv"
+write.csv(pid_inorder, file.path(datapath, freq_outputfile))
 
-pid_outputfile <- "proteins_by_number_pids.csv"
-write.csv(pid_inorder, file.path(datapath, pid_outputfile))
+pid_outputfile <- "all_proteins_with_pids.csv"
+write.csv(pid_proteins_by_name, file.path(datapath, pid_outputfile))
+
