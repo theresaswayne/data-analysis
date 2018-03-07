423f880606c6b226f10dd462244f2be3fc80d12c
theresaswayne <tcs6@columbia.edu>
2018-03-07 16:03:43 -0500

    domains: filtering by PID list in separate file

--- R/count_domains.R
+++ R/count_domains.R
@@ -2,8 +2,8 @@
 # Script to count number of detected domains per protein
 # in records downloaded from SGD
 
-# Input: CSV file from SGD with human-readable headers
-# Must include Protein systematic name, 
+# Input: CSV file from SGD with raw headers
+# Must include Protein systematic name (secondary identifier), 
 # and all possible fields under protein domains.
 # Assumes that the protein contains some domains.
 
@@ -25,36 +25,60 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 
-# Read file ---------------------------------------------------------------
+# Read files ---------------------------------------------------------------
 
-# datapath <- "/Users/theresa/Documents/home-github/data-analysis/R/count_domains_data"
-datapath <- "/Users/confocal/github_theresaswayne/data-analysis/R/count_domains_data"
-datafile <- "20180221_SGDproteins_atleast1domain.csv"
+# datapath <- "/Users/theresa/Documents/home-github/data-analysis/R/count_domains_data" # for home
+datapath <- "/Users/confocal/github_theresaswayne/data-analysis/R/count_domains_data" # for work
+datafile <- "20180302_SGDproteins_atleast1domain.csv"
+pidfile <- "PID_accessions_only.csv"
 
 # unlike base R's read.csv, readr's read_csv gives a tbl that can be grouped
 proteins <- read_csv(file.path(datapath, datafile)) 
 
+# a list of accession numbers, with header "Accession"
+pids <- read_csv(file.path(datapath, pidfile))
+
+# change tbl to vector
+pids_acc <- pids$Accession
+
+pid_proteins <- filter(proteins, Gene.proteins.proteinDomains.name %in% pids_acc)
+
 # Group by protein name ---------------------------------------------------
 
 proteins_by_protname <- group_by(proteins, proteins$`Gene.proteins.secondaryIdentifier`)
+pid_proteins_by_name <- group_by(pid_proteins, pid_proteins$`Gene.proteins.secondaryIdentifier`)
 
 # Count rows bearing each protein name  --------
 
 domains_per_protein <- summarize(proteins_by_protname, n())
+pid_domains_per_protein <- summarize(pid_proteins_by_name, n())
 
 # use backticks around the column name because it has parentheses
 inorder <- arrange(domains_per_protein, desc(`n()`))
+pid_inorder <- arrange(pid_domains_per_protein, desc(`n()`))
 
 # Create and save histogram of domains per protein ---------------------------------
 
 outputgraph <- file.path(datapath,"domains_per_prot_redundant.pdf")
 pdf(file = outputgraph)
+hist(domains_per_protein$`n()`, 
+     col = "blue", 
+     main="Domains per protein including redundancies", 
+     xlab="domains identified in SGD")
+dev.off() # closes pdf output
 
-hist(domains_per_protein$`n()`, breaks = 70, col = "blue", main="Domains per protein including redundancies", xlab="domains identified in SGD")
-
+pid_outputgraph <- file.path(datapath,"pid_domains_per_prot_redundant.pdf")
+pdf(file = pid_outputgraph)
+hist(pid_domains_per_protein$`n()`, 
+     col = "blue", 
+     main="PIDs per protein including redundancies", 
+     xlab="PIDs identified in SGD")
 dev.off() # closes pdf output
 
 # Save data table -----------------------------------------------
 
 outputfile <- "proteins_by_number_domains.csv"
 write.csv(inorder, file.path(datapath,outputfile))
+
+pid_outputfile <- "proteins_by_number_pids.csv"
+write.csv(pid_inorder, file.path(datapath, pid_outputfile))
