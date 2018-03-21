cf8b8a782c260ac2825649964f8af07f52797e5c
Theresa Swayne <theresa@Mini-Swemtage.local>
2018-03-21 18:34:32 -0400

    R -- usage -- updated script and data

--- R/usage.R
+++ R/usage.R
@@ -7,17 +7,20 @@ library(readr)
 # Import data ------
 
 datapath <- "/Users/confocal/github_theresaswayne/data-analysis/R/usage_data"
+datapath <- "/Users/theresa/Documents/home-github/data-analysis/R/usage_data"
 #datafile <- "09012017_01312018_charges_report_source_data.csv"
-datafile <- "03012017_03012018_charges_report_source_data.csv"
+datafile <- "01012018_03012018_charges_report_source_data.csv"
 
 # unlike base R's read.csv, readr's read_csv gives a tbl that can be grouped
 usage <- read_csv(file.path(datapath, datafile)) # errors result, but seems ok
 
-# Filter by date ----------------------------------------------------------
+# Optional filter by date ----------------------------------------------------------
 #usage <- filter(usage, `Completion Date` > "2018-01-01")
 
-# Calculate hours based on Usage Type column ------------------------------
 
+# Assisted vs Non-assisted ------------------------------------------------
+
+# Calculate hours based on Usage Type column
 hrs_assist <- dplyr::filter(usage, grepl("Assisted|Training|1", `Usage Type`)) %>%
   dplyr::select(Quantity) %>%
   sum()
@@ -26,16 +29,40 @@ hrs_unass <- dplyr::filter(usage, grepl("Unassisted|Independent", `Usage Type`))
   dplyr::select(Quantity) %>%
   sum()
 
-
-# Summarize ---------------------------------------------------------------
-
+# Summarize
 usage_type <- c("Assisted/Service", "Non-Assisted")
 hours <- c(hrs_assist, hrs_unass)
 
-hrs_summ <- cbind(`Usage Type` = usage_type, Hours = hours) %>%
+hrs_assist_summ <- cbind(`Usage Type` = usage_type, Hours = hours) %>%
   as.data.frame(stringsAsFactors = FALSE)
 
-hrs_summ$Hours <- as.numeric(as.character(hrs_summ$Hours)) # force R to see the hours as numeric
+hrs_assist_summ$Hours <- as.numeric(as.character(hrs_assist_summ$Hours)) # force R to see the hours as numeric
 
-hrs_summ <- mutate(hrs_summ, 
+hrs_assist_summ <- mutate(hrs_assist_summ, 
+                   Percent = round((Hours / sum(Hours)), 2))
+
+
+# HICCC vs non  ------------------------------------------------------------
+
+eqpt_service_usage <- dplyr::filter(usage, !grepl("Lenses", `Charge Name`))
+
+hrs_HICCC <- dplyr::filter(eqpt_service_usage, grepl("Cancer Center Member", `Price Type`)) %>%
+  dplyr::select(Quantity) %>%
+  sum()
+
+hrs_InternalNonHICCC <- dplyr::filter(eqpt_service_usage, grepl("Dept of Pathology|Internal - Columbia", `Price Type`)) %>%
+  dplyr::select(Quantity) %>%
+  sum()
+
+# users_HICCC <-
+# Summarize
+price_type <- c("HICCC", "Non-HICCC Internal")
+hoursCC <- c(hrs_HICCC, hrs_InternalNonHICCC)
+
+hrs_cc_summ <- cbind(`Price Type` = price_type, Hours = hoursCC) %>%
+  as.data.frame(stringsAsFactors = FALSE)
+
+hrs_cc_summ$Hours <- as.numeric(as.character(hrs_cc_summ$Hours)) # force R to see the hours as numeric
+
+hrs_cc_summ <- mutate(hrs_cc_summ, 
                    Percent = round((Hours / sum(Hours)), 2))
