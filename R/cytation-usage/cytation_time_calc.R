0dc756995ec9ee6c9ab3d83e608b238a688afe28
Theresa Swayne <tswayne@mac.com>
2019-03-12 16:51:49 -0400

    Update cytation_time_calc.R
    - added file chooser
    - identified start and end rows
    - subtracted time and placed in column

--- R/cytation-usage/cytation_time_calc.R
+++ R/cytation-usage/cytation_time_calc.R
@@ -2,15 +2,36 @@
 
 # note -- include only the audit trail. No procedure sumamry.
 
-
-require(readr)
+require(tidyverse) # for reading and parsing
+require(tcltk) # for file choosing
 
 # load data
-# TODO: make a file chooser
-X20190311_test_imaging_time_protocol_2_190311_165300 <- read_csv("sample-data/20190311 test imaging time protocol 2_190311_165300.txt", 
-                                                                 col_types = cols(Comment = col_character(), 
-                                                                                  Date = col_datetime(format = "%m/%d/%Y %H:%M:%S %p")), 
-                                                                 skip = 2)
-# TODO: Find "plate read started" and "plate read successfully completed" (sort into columns for multi-step expts?)
 
-# TODO: subtract these values and report a total for the whole expt
+#X20190311_test_imaging_time_protocol_2_190311_165300 <- read_csv("sample-data/20190311 test imaging time protocol 2_190311_165300.txt", 
+                                                                 # col_types = cols(Comment = col_character(), 
+                                                                 #                  Date = col_datetime(format = "%m/%d/%Y %H:%M:%S %p")), 
+                                                                 # skip = 2)
+
+logfile <- tk_choose.files(default = "", caption = "Select the Audit Trail file", multi = FALSE) # file chooser window, with a message
+
+logdata <- read_csv(logfile,
+                    col_types = cols(Comment = col_character(), 
+                                     Date = col_datetime(format = "%m/%d/%Y %H:%M:%S %p")), 
+                    skip = 2)
+
+
+startTimes <- logdata %>% 
+  filter(Event == "Plate read started") %>%
+  select(Start = Date)
+
+
+endTimes <- logdata %>% 
+  filter(Event == "Plate read successfully completed") %>%
+  select(End = Date) # the columns need to be renamed before combining in tidyverse
+
+
+scanTimes <- as.tbl(cbind(startTimes, endTimes))
+
+scanTimes <- scanTimes %>% 
+  mutate(elapsedTime = End - Start)
+
