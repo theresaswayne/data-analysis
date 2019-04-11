618299fdc40e9f1a7bb5f869b1c91a0c80dc60e3
theresaswayne <tcs6@columbia.edu>
2019-04-11 15:33:28 -0400

    Update usage.R
    improved comments, slightly reorganized, added TODOs

--- R/facility-usage/usage.R
+++ R/facility-usage/usage.R
@@ -1,29 +1,24 @@
 # usage.R
-# pulling useful data from iLab usage tables
+# From iLab usage table, calculates:
+# -- assisted vs. non-assisted hours, and fraction of total
+# -- HICCC vs. non-HICCC hours and number of labs, and fraction of total
+# Theresa Swayne
 
 require(tidyverse)
 
-# Import data ------
+# Import data  ------
+# Must be a "source_data" CSV file from iLab
 
-datafile <- file.choose() # opens a file chooser window, no message
-
-#datapath <- "/Users/confocal/github_theresaswayne/data-analysis/R/usage_data"
-#datapath <- "/Users/theresa/Documents/home-github/data-analysis/R/usage_data"
-#datafile <- "09012017_01312018_charges_report_source_data.csv"
-#datafile <- "01012018_03012018_charges_report_source_data.csv"
-#datafile <- "01012017_01012018_charges_report_source_data.csv"
-
-# unlike base R's read.csv, readr's read_csv gives a tbl that can be grouped
-#usage <- read_csv(file.path(datapath, datafile)) # errors result, but seems ok
+datafile <- file.choose() # opens a file chooser window
 usage <- read_csv(file.path(datafile)) # errors result, but seems ok
 
 # Optional filter by date ----------------------------------------------------------
 #usage <- filter(usage, `Completion Date` > "2018-03-01")
 
 
-# Assisted vs Non-assisted ------------------------------------------------
+# Assisted vs Non-assisted ----------------------------------------
 
-# Calculate hours based on Usage Type column
+# Calculate hours for different Usage Types
 hrs_assist <- dplyr::filter(usage, grepl("Assisted|Training|1", `Usage Type`)) %>%
   dplyr::select(Quantity) %>%
   sum()
@@ -32,30 +27,36 @@ hrs_unass <- dplyr::filter(usage, grepl("Unassisted|Independent", `Usage Type`))
   dplyr::select(Quantity) %>%
   sum()
 
-# Summarize
+# Collect total hours by usage type
 usage_type <- c("Assisted/Service", "Non-Assisted")
 hours <- c(hrs_assist, hrs_unass)
 
 hrs_assist_summ <- cbind(`Usage Type` = usage_type, Hours = hours) %>%
   as.data.frame(stringsAsFactors = FALSE)
 
+# Calculate fraction of time assisted/non-assisted
+
 hrs_assist_summ$Hours <- as.numeric(as.character(hrs_assist_summ$Hours)) # force R to see the hours as numeric
 
 hrs_assist_summ <- mutate(hrs_assist_summ, 
-                   Percent = round((Hours / sum(Hours)), 2))
+                   Fraction = round((Hours / sum(Hours)), 2))
 
 
-# HICCC vs non  ------------------------------------------------------------
+# HICCC vs non-HICCC  -------------------------------------------------
+
+# eliminate lenses from this calculation
+
+# TODO: eliminate TC hood
 
 eqpt_service_usage <- dplyr::filter(usage, !grepl("Lenses", `Charge Name`))
+
+# Find total hours of HICCC use 
 
 hrs_HICCC <- dplyr::filter(eqpt_service_usage, grepl("Cancer Center Member", `Price Type`)) %>%
   dplyr::select(Quantity) %>%
   sum()
 
-hrs_NonHICCC <- dplyr::filter(eqpt_service_usage, grepl("Dept of Pathology|Internal - Columbia|External - Academic", `Price Type`)) %>%
-  dplyr::select(Quantity) %>%
-  sum()
+# Find number of HICCC labs
 
 appts_HICCC <- dplyr::filter(eqpt_service_usage, grepl("Cancer Center Member", `Price Type`)) %>%
   dplyr::select(`Customer Lab`)
@@ -63,13 +64,21 @@ appts_HICCC <- dplyr::filter(eqpt_service_usage, grepl("Cancer Center Member", `
 appts_HICCC$`Customer Lab` <- factor(appts_HICCC$`Customer Lab`)
 users_HICCC <- nlevels(appts_HICCC$`Customer Lab`)
 
+# Find total hours of non-HICCC use 
+
+hrs_NonHICCC <- dplyr::filter(eqpt_service_usage, grepl("Dept of Pathology|Internal - Columbia|External - Academic", `Price Type`)) %>%
+  dplyr::select(Quantity) %>%
+  sum()
+
+# Find number of non-HICCC labs
+
 appts_NonHICCC <- dplyr::filter(eqpt_service_usage, grepl("Dept of Pathology|Internal - Columbia|External - Academic", `Price Type`)) %>%
   dplyr::select(`Customer Lab`)
 
 appts_NonHICCC$`Customer Lab` <- factor(appts_NonHICCC$`Customer Lab`)
 users_NonHICCC <- nlevels(appts_NonHICCC$`Customer Lab`)
 
-# Summarize
+# Collect data in a table
 price_type <- c("HICCC", "Non-HICCC")
 hoursCC <- c(hrs_HICCC, hrs_NonHICCC)
 usersCC <- c(users_HICCC, users_NonHICCC)
@@ -80,9 +89,12 @@ hrs_cc_summ <- cbind(`Price Type` = price_type, Hours = hoursCC, Labs = usersCC)
 hrs_cc_summ$Hours <- as.numeric(as.character(hrs_cc_summ$Hours)) # force R to see the hours as numeric
 
 hrs_cc_summ <- mutate(hrs_cc_summ, 
-                   HrsPercent = round((Hours / sum(Hours)), 2))
+                   HrsFraction = round((Hours / sum(Hours)), 2))
 
 hrs_cc_summ$Labs <- as.numeric(as.character(hrs_cc_summ$Labs)) # force R to see the labs as numeric
 
 hrs_cc_summ <- mutate(hrs_cc_summ, 
-                      LabsPercent = round((Labs / sum(Labs)), 2))
+                      LabsFraction = round((Labs / sum(Labs)), 2))
+
+#TODO: Add total hours as a row in the summary tables
+#TODO: Write summary as CSV (using source filename as a base name)
